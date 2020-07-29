f1c4ea8d4d34a6586a6470deb5b12d2450a79dca
David Ronk <david@expectedbehavior.com>
2020-07-29 12:19:42 -0400

    Switch back to single response object.
    This reverts commit c0e40d0cbd1b290cb7e61718f75b0abbb7233eb5.
    
    This commit was designed to increase backwards compatibility but after
    further discussion it was decided that the added complexity was less
    desirable.  Additionally there are already breaking changes in this new
    version that will need address and this would be a minor one.

--- docraptor/__init__.py
+++ docraptor/__init__.py
@@ -23,7 +23,6 @@ from docraptor.api_client import ApiClient
 from docraptor.configuration import Configuration
 # import models into sdk package
 from docraptor.models.async_doc import AsyncDoc
-from docraptor.models.async_doc_status import AsyncDocStatus
 from docraptor.models.doc import Doc
-from docraptor.models.prince_options import PrinceOptions
 from docraptor.models.doc_status import DocStatus
+from docraptor.models.prince_options import PrinceOptions
