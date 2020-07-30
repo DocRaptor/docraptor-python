086e0bd0c3ad2e5308a574ca75917c18561551f7
Alex Overbeck <alex@expectedbehavior.com>
2020-07-30 15:13:53 -0400

    Updates setup.py to include the correct information
    Why is this change needed?
    --------------------------
    When swagger regenerated our code it overwrote our setup.py script. This
    commit brings back the important pieces that are relevant to DocRaptor.
    
    Co-authored-by: David Ronk <david@expectedbehavior.com>

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 """
 
 
-from setuptools import setup, find_packages  # noqa: H301
+from setuptools import setup, find_packages
 
 NAME = "docraptor"
 VERSION = "2.0.0"
@@ -33,14 +33,39 @@ REQUIRES = [
 setup(
     name=NAME,
     version=VERSION,
-    description="DocRaptor",
-    author_email="",
-    url="",
-    keywords=["Swagger", "DocRaptor"],
+    description="A wrapper for the DocRaptor HTML to PDF/XLS service.",
+    author="Expected Behavior",
+    maintainer="DocRaptor Support",
+    maintainer_email="support@docraptor.com",
+    url="https://github.com/docraptor/docraptor-python",
+    keywords=["DocRaptor", "API", "PDF", "XLS", "XLSX", "HTML"],
+    classifiers=[ # see https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 5 - Production/Stable",
+
+        "Environment :: Web Environment",
+
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+
+        "License :: OSI Approved :: Apache Software License",
+
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Multimedia :: Graphics :: Presentation",
+        "Topic :: Office/Business",
+        "Topic :: Office/Business :: Financial :: Spreadsheet",
+
+        "Topic :: Printing",
+        "Topic :: Text Processing :: Markup :: HTML",
+    ],
+    license="Apache V2.0 License",
     install_requires=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
     long_description="""\
-    A native client library for the DocRaptor HTML to PDF/XLS service.  # noqa: E501
+    A native client library for the DocRaptor HTML to PDF/XLS service.
     """
 )
