93fc41a81666b14ac444a99b466cdc98e641ae14
James Paden <james@jamespaden.com>
2022-08-31 15:15:41 -0400

    Update with latest code generator files

--- examples/hosted_sync.py
+++ examples/hosted_sync.py
@@ -1,47 +1,25 @@
-# As a paid add-on, DocRaptor can provide long-term, publicly-accessible hosting for your documents.
-# This allows you to provide a URL to your end users, third party tools like Zapier and Salesforce,
-# or anyone else. We'll host the document on your behalf at a completely unbranded URL for as long
-# as you want, or within the limits you specify.
-#
-# This example demonstrates creating a PDF that DocRaptor will host for you using common options.
-# By default, hosted documents do not have limits on downloads or hosting time, though you may
-# pass additional parameters to the document generation call to set your own limits. Learn more
-# about the specific options in the hosted API documentation.
-# https://docraptor.com/documentation/api#api_hosted
-#
-# It is created synchronously, which means DocRaptor will allow it to generate for up to 60 seconds.
-# Since this document will be hosted by DocRaptor the response from this request will return a JSON
-# formatted object containing public URL where the document can be downloaded from.
-#
-# DocRaptor supports many options for output customization, the full list is
-# https://docraptor.com/documentation/api#api_general
-#
-# You can run this example with: python sync.rb
-
 import docraptor
-import shutil
 
 doc_api = docraptor.DocApi()
+# this key works in test mode!
 doc_api.api_client.configuration.username = 'YOUR_API_KEY_HERE'
-# doc_api.api_client.configuration.debug = True
 
 try:
+    # different method than the non-hosted documents
+    response = doc_api.create_hosted_doc({
+        'test': True,  # test documents are free but watermarked
+        'document_type': 'pdf',
+        'document_content': '<html><body>Hello World!</body></html>',
+        # 'document_url': 'http://docraptor.com/examples/invoice.html',
+        # 'javascript': False,
+        # 'prince_options': # {
+        #    'media': 'print', # @media 'screen' or 'print' CSS
+        #    'baseurl': 'https://yoursite.com', # the base URL for any relative URLs
+        # },
+    })
 
-  create_response = doc_api.create_hosted_doc({
-    "test": True,                                                   # test documents are free but watermarked
-    "document_content": "<html><body>Hello World</body></html>",    # supply content directly
-    # "document_url": "http://docraptor.com/examples/invoice.html", # or use a url
-    "name": "docraptor-python.pdf",                                 # help you find a document later
-    "document_type": "pdf",                                         # pdf or xls or xlsx
-    # "javascript": True,                                           # enable JavaScript processing
-    # "prince_options": {
-    #   "media": "screen",                                          # use screen styles instead of print styles
-    #   "baseurl": "http://hello.com",                              # pretend URL when using document_content
-    # },
-  })
-  print(f"The hosted PDF is now available for public download at {create_response.download_url}")
-
+    print(f"The PDF is hosted at {response.download_url}")
 except docraptor.rest.ApiException as error:
-  print(error.status)
-  print(error.reason)
-  print(error.body)
+    print(error.status)
+    print(error.reason)
+    print(error.body)
--- examples/sync.py
+++ examples/sync.py
@@ -1,42 +1,29 @@
-# This example demonstrates creating a PDF using common options and saving it
-# to a place on the filesystem.
-#
-# It is created synchronously, which means DocRaptor will render it for up to
-# 60 seconds. It is slightly simpler than making documents using the async
-# interface but making many documents in parallel or very large documents with
-# lots of assets will require the async api.
-#
-# DocRaptor supports many options for output customization, the full list is
-# https://docraptor.com/documentation/api#api_general
-#
-# You can run this example with: python sync.rb
-
 import docraptor
-import shutil
 
 doc_api = docraptor.DocApi()
-doc_api.api_client.configuration.username = 'YOUR_API_KEY_HERE' # this key works for test documents
-# doc_api.api_client.configuration.debug = True
+# this key works in test mode!
+doc_api.api_client.configuration.username = 'YOUR_API_KEY_HERE'
 
 try:
+    response = doc_api.create_doc({
+        'test': True,  # test documents are free but watermarked
+        'document_type': 'pdf',
+        'document_content': '<html><body>Hello World!</body></html>',
+        # 'document_url': 'http://docraptor.com/examples/invoice.html',
+        # 'javascript': False,
+        # 'prince_options': # {
+        #    'media': 'print', # @media 'screen' or 'print' CSS
+        #    'baseurl': 'https://yoursite.com', # the base URL for any relative URLs
+        # },
+    })
 
-  create_response = doc_api.create_doc({
-    "test": True,                                                   # test documents are free but watermarked
-    "document_content": "<html><body>Hello World</body></html>",    # supply content directly
-    # "document_url": "http://docraptor.com/examples/invoice.html", # or use a url
-    "name": "docraptor-python.pdf",                                 # help you find a document later
-    "document_type": "pdf",                                         # pdf or xls or xlsx
-    # "javascript": True,                                           # enable JavaScript processing
-    # "prince_options": {
-    #   "media": "screen",                                          # use screen styles instead of print styles
-    #   "baseurl": "http://hello.com",                              # pretend URL when using document_content
-    # },
-  })
-  with open("/tmp/docraptor-python.pdf", "wb") as f:
-    f.write(create_response)
-  print("Wrote PDF to /tmp/docraptor-python.pdf")
-
+    # create_doc() returns a binary string
+    with open('docraptor-hello.pdf', 'w+b') as f:
+        binary_formatted_response = bytearray(response)
+        f.write(binary_formatted_response)
+        f.close()
+    print('Successfully created docraptor-hello.pdf!')
 except docraptor.rest.ApiException as error:
-  print(error.status)
-  print(error.reason)
-  print(error.body)
+    print(error.status)
+    print(error.reason)
+    print(error.body)
