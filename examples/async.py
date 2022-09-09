b486225cc2cfd22a53d42c32bb5f27ed49a01f40
James Paden <james@jamespaden.com>
2022-09-09 17:11:07 -0400

    Update with latest code generator files

--- examples/async.py
+++ examples/async.py
@@ -5,50 +5,48 @@
 # 10 minutes. This is useful when creating many documents in parallel, or very
 # large documents with lots of assets.
 #
-# DocRaptor supports many options for output customization, the full list is
-# https://docraptor.com/documentation/api#api_general
+# DocRaptor supports many CSS and API options for output customization. Visit
+# https://docraptor.com/documentation/ for full details.
 #
-# You can run this example with: python async.py
+# You can run this example with: python Async.py
 
 import docraptor
 import time
-import shutil
 
 doc_api = docraptor.DocApi()
-doc_api.api_client.configuration.username = 'YOUR_API_KEY_HERE' # this key works for test documents
-# doc_api.api_client.configuration.debug = True
+# this key works in test mode!
+doc_api.api_client.configuration.username = 'YOUR_API_KEY_HERE'
 
 try:
+    # different method than the synchronous documents
+    response = doc_api.create_async_doc({
+        'test': True,  # test documents are free but watermarked
+        'document_type': 'pdf',
+        'document_content': '<html><body>Hello World!</body></html>',
+        # 'document_url': 'https://docraptor.com/examples/invoice.html',
+        # 'javascript': False,
+        # 'prince_options': # {
+        #    'media': 'print', # @media 'screen' or 'print' CSS
+        #    'baseurl': 'https://yoursite.com', # the base URL for any relative URLs
+        # },
+    })
 
-  create_response = doc_api.create_async_doc({
-    "test": True,                                                   # test documents are free but watermarked
-    "document_content": "<html><body>Hello World</body></html>",    # supply content directly
-    # "document_url": "http://docraptor.com/examples/invoice.html", # or use a url
-    "name": "docraptor-python.pdf",                                 # help you find a document later
-    "document_type": "pdf",                                         # pdf or xls or xlsx
-    # "javascript": True,                                           # enable JavaScript processing
-    # "prince_options": {
-    #   "media": "screen",                                          # use screen styles instead of print styles
-    #   "baseurl": "http://hello.com",                              # pretend URL when using document_content
-    # },
-  })
-
-  while True:
-    status_response = doc_api.get_async_doc_status(create_response.status_id)
-    if status_response.status == "completed":
-      doc_response = doc_api.get_async_doc(status_response.download_id)
-      with open("/tmp/docraptor-python.pdf", "wb") as f:
-        f.write(doc_response)
-      print("Wrote PDF to /tmp/docraptor-python.pdf")
-      break
-    elif status_response.status == "failed":
-      print("FAILED")
-      print(status_response)
-      break
-    else:
-      time.sleep(1)
-
+    while True:
+        status_response = doc_api.get_async_doc_status(response.status_id)
+        if status_response.status == "completed":
+            pdf = doc_api.get_async_doc(status_response.download_id)
+            # get_async_doc() returns a binary string
+            with open("github-async.pdf", "wb") as f:
+                f.write(pdf)
+            print('Wrote PDF to github-async.pdf')
+            break
+        elif status_response.status == "failed":
+            print("FAILED")
+            print(status_response)
+            break
+        else:
+            time.sleep(1)
 except docraptor.rest.ApiException as error:
-  print(error.status)
-  print(error.reason)
-  print(error.body)
+    print(error.status)
+    print(error.reason)
+    print(error.body)
--- examples/sync.py
+++ examples/sync.py
@@ -6,37 +6,37 @@
 # interface but making many documents in parallel or very large documents with
 # lots of assets will require the async api.
 #
-# DocRaptor supports many options for output customization, the full list is
-# https://docraptor.com/documentation/api#api_general
+# DocRaptor supports many CSS and API options for output customization. Visit
+# https://docraptor.com/documentation/ for full details.
 #
-# You can run this example with: python sync.rb
+# You can run this example with: python Sync.py
 
 import docraptor
-import shutil
 
 doc_api = docraptor.DocApi()
-doc_api.api_client.configuration.username = 'YOUR_API_KEY_HERE' # this key works for test documents
-# doc_api.api_client.configuration.debug = True
+# this key works in test mode!
+doc_api.api_client.configuration.username = 'YOUR_API_KEY_HERE'
 
 try:
+    response = doc_api.create_doc({
+        'test': True,  # test documents are free but watermarked
+        'document_type': 'pdf',
+        'document_content': '<html><body>Hello World!</body></html>',
+        # 'document_url': 'https://docraptor.com/examples/invoice.html',
+        # 'javascript': False,
+        # 'prince_options': # {
+        #    'media': 'print', # @media 'screen' or 'print' CSS
+        #    'baseurl': 'https://yoursite.com', # the base URL for any relative URLs
+        # },
+    })
 
-  create_response = doc_api.create_doc({
-    "test": True,                                                   # test documents are free but watermarked
-    "document_content": "<html><body>Hello World</body></html>",    # supply content directly
-    # "document_url": "http://docraptor.com/examples/invoice.html", # or use a url
-    "name": "docraptor-python.pdf",                                 # help you find a document later
-    "document_type": "pdf",                                         # pdf or xls or xlsx
-    # "javascript": True,                                           # enable JavaScript processing
-    # "prince_options": {
-    #   "media": "screen",                                          # use screen styles instead of print styles
-    #   "baseurl": "http://hello.com",                              # pretend URL when using document_content
-    # },
-  })
-  with open("/tmp/docraptor-python.pdf", "wb") as f:
-    f.write(create_response)
-  print("Wrote PDF to /tmp/docraptor-python.pdf")
-
+    # create_doc() returns a binary string
+    with open('github-sync.pdf', 'w+b') as f:
+        binary_formatted_response = bytearray(response)
+        f.write(binary_formatted_response)
+        f.close()
+    print('Successfully created github-sync.pdf!')
 except docraptor.rest.ApiException as error:
-  print(error.status)
-  print(error.reason)
-  print(error.body)
+    print(error.status)
+    print(error.reason)
+    print(error.body)
